255eb41287b7cf95345ef9074a23db127b44caa2
Caio Gonçalves Vieira <caiogoncalvesvieira11@gmail.com>
2025-06-25 12:50:01 -0300

    back INCOMPLETO e ajustes no front

--- criginahair_site/src/app/contato/contato.component.ts
+++ criginahair_site/src/app/contato/contato.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
+  http: any;
+  onSubmit(form: NgForm) {
+  const dados = {
+    nome: form.value.nome,
+    telefone: form.value.telefone,
+    servico: form.value.servico,
+    dataPreferida: form.value.data
+  };
 
-}
+  this.http.post('http://localhost:4200/#contato', dados)
+    .subscribe({
+      next: (res: any) => alert('Mensagem enviada no WhatsApp!'),
+      error: () => alert('Erro ao enviar.')
+    });
+}}
